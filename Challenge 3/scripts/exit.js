7cb578573236be5202a0119a4bd367f23b28620c
zeynepkarakaya <46029791+zeynepkarakaya@users.noreply.github.com>
2021-06-20 20:04:28 +0200

    Delete Challenge 3 directory

--- Challenge 3/scripts/exit.js
+++
@@ -1,6 +0,0 @@
-var blackscreen = document.getElementById('programname');
-
-blackscreen.onmousemove = function(){
-document.getElementById('message').classList.add('white');
-document.getElementById('footer').classList.add('white');
-};
--- Challenge 3/scripts/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('goButton').onclick = function () {
-  document.getElementById('goButton').classList.add('hidden');
-  document.getElementById('usa').classList.remove('hidden');
-  document.getElementById('china').classList.remove('hidden');
-  document.getElementById('search').classList.remove('hidden');
-}
